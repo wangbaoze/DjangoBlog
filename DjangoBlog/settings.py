3edf4b4985c6de7f17d469f88f49e5e92f647128
wangbaoze <18234034790@163.com>
2018-01-03 19:46:14 +0800

    nginx

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -149,7 +149,7 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 # 允许使用用户名或密码登录
 AUTHENTICATION_BACKENDS = ['accounts.user_login_backend.EmailOrUsernameModelBackend']
 
-STATIC_ROOT = os.path.join(SITE_ROOT, 'collectedstatic')
+STATIC_ROOT = '/var/www/DjangoBlog/static/'
 
 STATIC_URL = '/static/'
 STATICFILES = os.path.join(BASE_DIR, 'static')
